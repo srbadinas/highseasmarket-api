7686460fef235d4197e47ff0255300a756f91ba0
Sebastian Badinas <srbadinas@gmail.com>
2025-01-02 18:56:16 +0800

    added postgresql ssl config

--- db.js
+++ db.js
@@ -9,7 +9,10 @@ const pool = new Pool({
     port: process.env.PG_PORT,
     user: process.env.PG_USERNAME,
     password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE
+    database: process.env.PG_DATABASE,
+    ssl: {
+        rejectUnauthorized: false,
+    }
 });
 
 export const query = async (sql, values) => {
